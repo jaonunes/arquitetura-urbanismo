2f143a560d59ba57bccd75c7f29c80789da5fe8b
João <jp6nunes@gmail.com>
2020-04-05 20:39:16 -0200

    Alteração no JS, criação de uma animation para aparecer os itens conforme o scroll. Falta responsividade

--- js/animacao.js
+++ js/animacao.js
@@ -7,7 +7,7 @@ $(function() {
     });
   });
 
-  $(function(){   
+ $(function(){   
     var nav = $('#nav-main');   
     $(window).scroll(function () { 
         if ($(this).scrollTop() > 790) { 
@@ -19,3 +19,43 @@ $(function() {
         } 
     });  
 });
+
+
+var root = document.documentElement;
+root.className += ' js';
+
+function boxTop(idBox) {
+	var boxOffset = $(idBox).offset().top;
+	return boxOffset;
+}
+
+$(document).ready(function() {
+	var $target = $('.anime'),
+			animationClass = 'anime-init',
+			windowHeight = $(window).height(),
+			offset = windowHeight - (windowHeight / 4);
+
+	function animeScroll() {
+		var documentTop = $(document).scrollTop();
+		$target.each(function() {
+			if (documentTop > boxTop(this) - offset) {
+				$(this).addClass(animationClass);
+			} else {
+				$(this).removeClass(animationClass);
+			}
+		});
+	}
+	animeScroll();
+
+	$(document).scroll(function() {
+		setTimeout(function() {animeScroll()}, 150);
+	});
+});
+
+$("#wpp").hover(
+	function() {
+	   $('#texto-wpp').collapse('show');
+	 }, function() {
+	   $('#texto-wpp').collapse('hide');
+	 }
+   );
