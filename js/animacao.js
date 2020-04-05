7de63aa974a5649892ad76fb85a1cf480f8283a1
João <jp6nunes@gmail.com>
2020-04-05 03:28:08 -0200

    Inicio da criação do Site com função de scrool e fixação do menu

---
+++ js/animacao.js
@@ -0,0 +1,21 @@
+$(function() {
+    $('.animation-ancora').bind('click',function(event){
+      var $anchor = $(this);
+      $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1000,'swing');
+      // Outras Animações
+      // linear, swing, jswing, easeInQuad, easeInCubic, easeInQuart, easeInQuint, easeInSine, easeInExpo, easeInCirc, easeInElastic, easeInBack, easeInBounce, easeOutQuad, easeOutCubic, easeOutQuart, easeOutQuint, easeOutSine, easeOutExpo, easeOutCirc, easeOutElastic, easeOutBack, easeOutBounce, easeInOutQuad, easeInOutCubic, easeInOutQuart, easeInOutQuint, easeInOutSine, easeInOutExpo, easeInOutCirc, easeInOutElastic, easeInOutBack, easeInOutBounce
+    });
+  });
+
+  $(function(){   
+    var nav = $('#nav-main');   
+    $(window).scroll(function () { 
+        if ($(this).scrollTop() > 790) { 
+            nav.addClass("menu-fixo");
+            $('body').css('padding-top', 70);
+        } else { 
+            nav.removeClass("menu-fixo"); 
+            $('body').css('padding-top', 0);
+        } 
+    });  
+});
